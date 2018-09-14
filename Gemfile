b653a7325403f81a36fb1b9cd06bf75568d30752
“Andrew <andrewwoods88@gmail.com>
2018-09-14 14:35:14 -0400

    installed gritter and fixed registrations controller for devise

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'devise'
 gem 'bootstrap', '~> 4.1', '>= 4.1.3'
 
 gem 'jquery-rails'
+
+gem 'gritter', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :first_name, :last_name
+  validates :username, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z]+([a-zA-Z]|\d)*\Z/ }
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+       
 end
