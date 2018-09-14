49e61320debded34108ccae7c2aeac4beea94298
“Andrew <andrewwoods88@gmail.com>
2018-09-14 14:24:35 -0400

    installed bootstrap 4 - stylized devise views

--- Gemfile
+++ Gemfile
@@ -54,3 +54,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+
+gem 'bootstrap', '~> 4.1', '>= 4.1.3'
+
+gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => {:registrations => "registrations"}
 
+  devise_scope :user do
+      get 'login', to: 'devise/sessions#new'
+      get 'signup', to: 'devise/registrations#new'
+    end  
+
   get 'pages/home'
 
 
